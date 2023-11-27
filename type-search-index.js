09a49a7805e4253efe57c82658826ca70b3ae102
LOOI LAI TYNG <laityng2000@gmail.com>
2023-11-27 18:32:22 +0800

    upload javadoc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"my.uum","l":"App"},{"p":"my.uum","l":"AppTest"},{"p":"my.uum","l":"Main"},{"p":"my.uum","l":"MyTelegramBot"},{"p":"my.uum","l":"Process"},{"p":"my.uum","l":"RoundRobinScheduler"},{"p":"my.uum","l":"SQLiteDBManager"}];updateSearchResults();
