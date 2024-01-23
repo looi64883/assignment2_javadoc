e70c9223d107067e488933c1f706af55aff5b307
LOOI LAI TYNG <laityng2000@gmail.com>
2024-01-23 23:48:59 +0800

    kafka html

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"my.uum"},{"l":"my.uum.RoundRobinAlgorithm"},{"l":"my.uum.SQLite"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"myapps"}]
